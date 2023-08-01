cb96f82dc734271aa276a86246a9e0c721d1d428
zhajinxing <zhajinxing@163.com>
2023-08-01 16:25:31 +0800

    V2000

--- app/components/ui-lib.tsx
+++ app/components/ui-lib.tsx
@@ -439,7 +439,7 @@ export function showImageModal(img: string) {
 
 export function showImage(img: string) {
   showModal({
-    title: Locale.Home.QrScan",
+    title: Locale.Home.QrScan,
     children: (
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
         <img
